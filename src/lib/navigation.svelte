66d352273897b6d240bd40e63f04d00c0bf12404
Nadja Weinzettl <nadjaweinzettl@MacBook-Pro-von-Nadja-2.local>
2023-03-14 11:07:28 +1300

    Starting the portfolio website project. Setting up an image folder, pages (footer, header, navigation) and layout page. Creating NAV bar

---
+++ src/lib/navigation.svelte
@@ -0,0 +1,6 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About Me</a>
+    <a href="/projects">Projects</a>
+    <a href="/contactme">Contact Me</a>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<!-- +layouts contains a layout that applies to every page -->
+<script>
+    import Navigation from "../lib/navigation.svelte";
+    import Footer from "../lib/footer.svelte";
+</script>
+
+
+<!-- include Nav bar -->
+<Navigation />
+
+<!-- include Footer -->
+<Footer />
