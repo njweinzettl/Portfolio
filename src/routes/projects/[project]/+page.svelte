c5819665ccfbfe0d25ae9d9abc4fe09a84b69338
Nadja Weinzettl <nadjaweinzettl@MBP-von-Nadja-2.eduroam.uod.otago.ac.nz>
2023-04-04 16:09:38 +1200

    Building projects for dynamic routing of project cards

---
+++ src/routes/projects/[project]/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+    import {page} from "$app/stores"
+    const project = $page.params.project //access projectname route parameter
+</script>
+
+<body>
+    <div>
+        <h1>{data.project.title}</h1>
+    </div>
+    <div> 
+        <h2> Description </h2>
+        <p> {data.project.description} </p>
+    </div>
+    
+    <div> 
+        <h2> My role </h2>
+        <p> {data.project.role} </p>
+    </div>
+    
+    <div> 
+        <h2> Process </h2>
+        <p> {data.project.process} </p>
+    </div>
+    
+    <div> 
+        <h2> Results </h2>
+        {data.project.results}
+    </div>    
+</body>
+
+
+<style>
+    div {
+        display: grid;
+        grid-template-columns: 2;
+    }
+</style>
--- src/routes/projects/[projectname]/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    export let data;
-    import {page} from "$app/stores"
-    const projectname = $page.params.projectname //access projectname route parameter
-</script>
-
-<h1>{data.title}</h1>
-<h2>Details about the project: {projectname}</h2>
